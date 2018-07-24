36e56dde6fcea4d6e5e6d576f3f4294e6fe5505a
Rodolfo Resines <rodolfo.sresines@gmail.com>
2018-07-23 19:54:55 -0500

    created the mvc and initial set up for the app

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("The Big Carl", false);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("The Big  Mac", false);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("The Big Wendy", false);
